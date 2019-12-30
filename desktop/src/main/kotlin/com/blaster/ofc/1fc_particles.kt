3970c83fe2c92271c39f254deff8c413d4198dd0
Grigory Zozulin <Grigory.Zozulin@soti.net>
2019-12-30 17:53:03 -0500

    ideas

--- desktop/src/main/kotlin/com/blaster/ofc/1fc_particles.kt
+++ desktop/src/main/kotlin/com/blaster/ofc/1fc_particles.kt
@@ -3,6 +3,10 @@ package com.blaster.ofc
 import com.blaster.platform.LwjglWindow
 import com.blaster.renderers.SimpleRenderer
 
+// todo: store the current position of particle in the texture object (100x100 tex == 10000 particles)
+// todo: gravity affected particles, ground plane
+// http://www.opengl-tutorial.org/intermediate-tutorials/billboards-particles/particles-instancing/
+
 private val simpleRenderer = SimpleRenderer()
 
 private val particles = object : LwjglWindow() {
