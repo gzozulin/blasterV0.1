44bf01c4b16bae05c018b8b9ed1e8a6fcf546355
Grigory Zozulin <Grigory.Zozulin@soti.net>
2019-12-31 13:41:48 -0500

    in progress

--- common_gl/src/main/kotlin/com/blaster/assets/PixelDecoder.kt
+++ common_gl/src/main/kotlin/com/blaster/assets/PixelDecoder.kt
@@ -1,31 +1,21 @@
 package com.blaster.assets
 
-import org.apache.commons.imaging.Imaging
 import java.awt.image.DataBufferByte
-import java.awt.image.DataBufferInt
 import java.io.InputStream
 import java.nio.ByteBuffer
-import java.nio.ByteOrder
+import javax.imageio.ImageIO
 
 open class PixelDecoder {
     data class Decoded(val pixels: ByteBuffer, val width: Int, val height: Int)
 
     open fun decodePixels(inputStream: InputStream): Decoded {
-        val bufferedImage = Imaging.getBufferedImage(inputStream)
+        val bufferedImage = ImageIO.read(inputStream)
         val byteBuffer: ByteBuffer
         when (val dataBuffer = bufferedImage.raster.dataBuffer) {
             is DataBufferByte -> {
                 val pixelData = dataBuffer.data
                 byteBuffer = ByteBuffer.allocateDirect(pixelData.size)
-                        .order(ByteOrder.nativeOrder())
-                        .put(pixelData)
-                byteBuffer.position(0)
-            }
-            is DataBufferInt -> {
-                val pixelData = dataBuffer.data
-                byteBuffer = ByteBuffer.allocateDirect(pixelData.size * 4)
-                        .order(ByteOrder.nativeOrder())
-                byteBuffer.asIntBuffer().put(pixelData)
+                byteBuffer.put(pixelData)
                 byteBuffer.position(0)
             }
             else -> throw IllegalArgumentException("Not implemented for data buffer type: " + dataBuffer.javaClass)
