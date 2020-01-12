f64194c8c059014a799ede000bb7305b7007095d
madeinsoviets <narkoyanp4p8x>
2020-01-11 20:43:10 -0500

    minor

--- common_gl/src/main/kotlin/com/blaster/gl/GlBuffer.kt
+++ common_gl/src/main/kotlin/com/blaster/gl/GlBuffer.kt
@@ -48,8 +48,8 @@ class GlBuffer(
         glCheck { backend.glBindBuffer(target, 0) }
     }
 
-    fun mapBuffer(access: Int): ByteBuffer = glCheck {  backend.glMapBuffer(target, access, buffer) }
-    fun unmapBuffer() = glCheck { backend.glUnapBuffer(target) }
+    private fun mapBuffer(access: Int): ByteBuffer = glCheck {  backend.glMapBuffer(target, access, buffer) }
+    private fun unmapBuffer() = glCheck { backend.glUnapBuffer(target) }
 
     fun updateBuffer(access : Int = backend.GL_WRITE_ONLY, update: (mapped: ByteBuffer) -> Unit) {
         val mapped = mapBuffer(access)
