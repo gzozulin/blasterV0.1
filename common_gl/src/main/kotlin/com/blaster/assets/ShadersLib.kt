6119aced2475962883c3a3aa08dc07ac5de10daa
madeinsoviets <narkoyanp4p8x>
2020-01-12 21:40:37 -0500

    ideas

--- common_gl/src/main/kotlin/com/blaster/assets/ShadersLib.kt
+++ common_gl/src/main/kotlin/com/blaster/assets/ShadersLib.kt
@@ -7,6 +7,9 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.nio.charset.Charset
 
+// todo: import common functions with placeholders, like:
+// import shaders/common/lookat
+
 class ShadersLib(private val assetStream: AssetStream) {
     fun loadProgram(vertShaderAsset: String, fragShaderAsset: String) : GlProgram = GlProgram(
             GlShader(GlShaderType.VERTEX_SHADER, slurpAsset(vertShaderAsset)),
